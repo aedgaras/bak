b1a0d1b282736746233ff9837f992086bd90a4bd
Edgaras <edgarasgreek@icloud.com>
2022-10-03 19:56:45 +0300

    Initial form

--- bak.ui/src/ui/authentication/LoginPage.tsx
+++ bak.ui/src/ui/authentication/LoginPage.tsx
@@ -1,3 +1,22 @@
+import { Box, Center, HStack } from '@chakra-ui/react';
+import { Formik } from 'formik';
 import { AppWrapper } from '../components/AppWrapper';
 
-export const LoginPage = () => <AppWrapper>Login</AppWrapper>;
+export const LoginPage = () => {
+    const initialValue = {};
+
+    return (
+        <AppWrapper>
+            <Center>
+                <Box padding={2} borderWidth={1}>
+                    <HStack>
+                        <Formik
+                            initialValues={initialValue}
+                            onSubmit={() => {}}
+                        ></Formik>
+                    </HStack>
+                </Box>
+            </Center>
+        </AppWrapper>
+    );
+};
