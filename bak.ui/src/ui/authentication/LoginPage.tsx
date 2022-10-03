2858c11fa2f95b2c51149497ff2f96dc1d211406
Edgaras <edgarasgreek@icloud.com>
2022-10-03 20:10:48 +0300

    Form validation

--- bak.ui/src/ui/authentication/LoginPage.tsx
+++ bak.ui/src/ui/authentication/LoginPage.tsx
@@ -1,19 +1,67 @@
-import { Box, Center, HStack } from '@chakra-ui/react';
-import { Formik } from 'formik';
+import {
+    Box,
+    Button,
+    Center,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    HStack,
+    Input,
+    Text,
+} from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
 import { AppWrapper } from '../components/AppWrapper';
 
 export const LoginPage = () => {
-    const initialValue = {};
+    const initialValue: { username: string; password: string } = {
+        username: '',
+        password: '',
+    };
+    const loginSchema = Yup.object().shape({
+        username: Yup.string().required('Required'),
+        password: Yup.string().required('Required'),
+    });
 
     return (
         <AppWrapper>
             <Center>
-                <Box padding={2} borderWidth={1}>
+                <Box padding={2} borderWidth={1} borderRadius={'lg'}>
+                    <Text>Login</Text>
                     <HStack>
                         <Formik
                             initialValues={initialValue}
-                            onSubmit={() => {}}
-                        ></Formik>
+                            validationSchema={loginSchema}
+                            onSubmit={(values) => {
+                                console.log(values);
+                            }}
+                        >
+                            {({ errors, touched }) => (
+                                <Form>
+                                    <FormControl>
+                                        <FormLabel>Name</FormLabel>
+                                        <Input name={'username'}></Input>
+                                        {errors.username && touched.username ? (
+                                            <FormErrorMessage>
+                                                {errors.username}
+                                            </FormErrorMessage>
+                                        ) : null}
+                                    </FormControl>
+                                    <FormControl>
+                                        <FormLabel>Password</FormLabel>
+                                        <Input name={'password'}></Input>
+                                        {errors.password && touched.password ? (
+                                            <FormErrorMessage>
+                                                {errors.password}
+                                            </FormErrorMessage>
+                                        ) : null}
+                                    </FormControl>
+                                    <FormControl>
+                                        <Button type={'submit'}>Submit</Button>
+                                    </FormControl>
+                                </Form>
+                            )}
+                        </Formik>
                     </HStack>
                 </Box>
             </Center>
