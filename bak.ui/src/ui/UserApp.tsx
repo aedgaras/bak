eb1dc08a5bc5866ba077c8c1077a1c2d5b558d8b
Edgaras <edgarasgreek@icloud.com>
2022-10-10 08:20:46 +0300

    main app

--- bak.ui/src/ui/UserApp.tsx
+++ bak.ui/src/ui/UserApp.tsx
@@ -1,13 +1,28 @@
+import { Center, Heading, Highlight } from '@chakra-ui/react';
 import { useContext } from 'react';
 import { UserContext } from '../context/UserContext';
 import { AppWrapper } from './components/AppWrapper';
-import { BoxWithShadow } from './components/BoxWithShadow';
 
 export const UserApp = () => {
     const userContext = useContext(UserContext);
     return (
         <AppWrapper>
-            <BoxWithShadow>userApp</BoxWithShadow>
+            <Center height={'100vh'}>
+                <Heading lineHeight="tall">
+                    <Highlight
+                        query={['spotlight', 'emphasize', 'Accentuate']}
+                        styles={{
+                            px: '2',
+                            py: '1',
+                            rounded: 'full',
+                            bg: 'teal.100',
+                        }}
+                    >
+                        With the Highlight component, you can spotlight,
+                        emphasize and accentuate words.
+                    </Highlight>
+                </Heading>
+            </Center>
         </AppWrapper>
     );
 };
